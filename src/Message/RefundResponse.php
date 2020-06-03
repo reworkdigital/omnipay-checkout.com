c370d9a1413c7b702c498a4ac233663eaf0845b9
Nicolas Maalouf <nicolas.maalouf@checkout.com>
2020-06-03 19:00:43 +0100

    Add refund functionality

---
+++ src/Message/RefundResponse.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Omnipay\CheckoutCom\Message;
+
+use Omnipay\Common\Message\ResponseInterface;
+
+class RefundResponse implements ResponseInterface
+{
+
+	private $data;
+	private $request;
+
+	public function __construct(RefundRequest $request, array $data)
+	{
+		$this->data = $data;
+		$this->request = $request;
+	}
+
+	public function getData()
+	{
+		return $this->data;
+	}
+
+	public function getRequest()
+	{
+		return $this->request;
+	}
+
+	public function isSuccessful()
+	{
+		if (!isset($this->getData()["action_id"])) {
+			return false;
+		}
+	}
+
+	public function isCancelled()
+	{
+		return !$this->isSuccessful();
+	}
+
+}
